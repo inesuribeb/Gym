1ada7354e62559b580b2bcc79172ce068892b53a
inesuribeb <inesuribeb@gmail.com>
2025-03-04 11:12:52 +0100

    general structure with main pages and main components

--- mi-aplicacion/src/Root.jsx
+++ mi-aplicacion/src/Root.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from './components/NavBar/Header'
+import Footer from './components/Footer/Footer'
 
-function Root () {
+function Root() {
     return (
-        <Outlet />
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
     )
 }
 
